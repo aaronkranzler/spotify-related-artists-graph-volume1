8c8bf4085632e919ec41066ac5795cff7a1c6111
System Administrator <root@Aarons-MacBook-Air.local>
2018-09-02 12:27:44 -0700

    Created ability to put user specified search term in url, and use spotify search api to get artist id to use

--- app.js
+++ app.js
@@ -8,15 +8,21 @@ var btoa = require('btoa');
 var client_id = process.env.SPOTIFY_CLIENT_ID;
 var client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 
+var currentToken = '';
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.get('/:id', function (req, res) {
-    makePostRequest(getAuthOptions(), req.params.id)
+app.get('/:artistName', function (req, res) {
+    getArtistIdFromSearchName(getAuthOptions(), req.params.artistName)
+        .then(function (result1) {
+	    //  There is going to only be one 'items' object in the list, because limit=1 in request   
+            return relatedArtistsRequest(result1.artists.items[0].id);
+        })
         .then(function (result) {
             const body = formatResponseByName(result);
-            res.send('Here are the 20 most related artists to the artist with the id ' + req.params.id + ': ' + body);
+            res.send('Here are the 20 closest related artists to the artist based on the search term \"' + req.params.artistName + '\": ' + body);
         });
 });
 
@@ -40,34 +46,75 @@ function getAuthOptions() {
     return authOptions;
 }
 
-function makePostRequest(authOptions, artistId) {
+function relatedArtistsRequest(artistId) {
+    return new Promise(
+        function (resolve, reject) {
+                var auth_header2 = `Bearer ${currentToken}`;
+                var options = {
+                    url: `https://api.spotify.com/v1/artists/${artistId}/related-artists`,
+                    headers: {
+                        'Authorization': auth_header2,
+                    },
+                    json: true
+                };
+                request.get(options, function (error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                          resolve(body);
+                    } else {
+                          console.log("ERROR: " + error + ", STATUS CODE: " + statusCode);
+                    }
+                });
+        }
+    );
+       
+}
+
+function getArtistIdFromSearchName(authOptions, query) {
     return new Promise(
         function (resolve, reject) {
             request.post(authOptions, function (error, response, body) {
                 if (!error && response.statusCode === 200) {
-                    // use the access token to access the Spotify Web API
-                    var token = body.access_token;
-                    var auth_header2 = `Bearer ${token}`;
+                    currentToken = body.access_token;
+                    let queryParam = query;
+                    if (query.indexOf(' ') >= 0) {
+                        queryParam = formatSearchQueryParam(query);
+                    }
+                    var auth_header2 = `Bearer ${currentToken}`;
                     var options = {
-                        url: `https://api.spotify.com/v1/artists/${artistId}/related-artists`,
+                        url: `https://api.spotify.com/v1/search?q=${queryParam}&type=artist&limit=1`,
                         headers: {
                             'Authorization': auth_header2,
                         },
                         json: true
-                    }
+                    }; 
                     request.get(options, function (error, response, body) {
-                        resolve(body);
+			if (!error && response.statusCode === 200) {
+                       		resolve(body);
+			} else {
+				console.log("ERROR: " + error + ", STATUS CODE: " + statusCode);
+			}
                     });
-                }
+                } else {
+			console.log("ERROR: " + error + ", STATUS CODE: " + statusCode);
+		}
             });
         }
     );
 }
 
+function formatSearchQueryParam(queryString) {
+    for (var i = 0; i < queryString.length; i++) {
+        if (queryString[i] === ' ') {
+            queryString = queryString.substring[0, i] + '+' + queryString.substring[i + 1, queryString.length];
+        }
+    }
+    return queryString;
+}
+
 function formatResponseByName(body) {
     let result = '';
     for (let i = 0; i < body.artists.length; i++) {
-        result += body.artists[i].name + ' ';
+        result += body.artists[i].name + ', ';
     }
     return result;
 }
